36cb55d70e9aca20879a84284942f5a60f383194
Pete Bunting <petebunting@mac.com>
2020-05-06 17:21:19 +0100

    Update split command to allow a deal base split rather than linear.

--- bin/splitcmdslist.py
+++ bin/splitcmdslist.py
@@ -34,12 +34,13 @@ import argparse
 import logging
 import math
 import os.path
+import tqdm
 
 import pbprocesstools.pbpt_utils
 
 logger = logging.getLogger('splitcmdslist.py')
 
-def splitByNumCommands(cmds_lst, n_out_cmds, outputfile):
+def splitByNumCommands(cmds_lst, n_out_cmds, outputfile, deal_split=False):
     """
 
     :param cmds_lst:
@@ -55,33 +56,60 @@ def splitByNumCommands(cmds_lst, n_out_cmds, outputfile):
 
     # Loop through and create individual files.
     out_file_lst = []
-    outfile_id = 1
-    for i in range(n_out_files):
-        l_bound = i * n_out_cmds
-        u_bound = (i + 1) * n_out_cmds
-        outfile_name = '{0}_{1}{2}'.format(outfile_base, outfile_id, outfile_ext)
-        logger.info('Creating file: {}.'.format(outfile_name))
-        pbpt_txt_utils.writeList2File(cmds_lst[l_bound:u_bound], outfile_name)
-        out_file_lst.append(outfile_name)
-        outfile_id = outfile_id + 1
+    if deal_split:
+        if n_remain > 0:
+            n_out_files = n_out_files + 1
 
-    if n_remain > 0:
-        # Output remaining cmds file
-        l_bound = n_out_files * n_out_cmds
-        u_bound = n_cmds
-        outfile_name = '{0}_{1}{2}'.format(outfile_base, outfile_id, outfile_ext)
-        logger.info('Creating file: {}.'.format(outfile_name))
-        pbpt_txt_utils.writeList2File(cmds_lst[l_bound:u_bound], outfile_name)
-        out_file_lst.append(outfile_name)
+        outfile_id = 1
+        split_lst = []
+        for i in range(n_out_files):
+            outfile_name = '{0}_{1}{2}'.format(outfile_base, outfile_id, outfile_ext)
+            logger.info('Creating file: {}.'.format(outfile_name))
+            out_file_lst.append(outfile_name)
+            i_info = dict()
+            i_info["file"] = outfile_name
+            i_info["cmds"] = list()
+            outfile_id = outfile_id + 1
+            split_lst.append(i_info)
+
+        c_lst = 0
+        for cmd in tqdm.tqdm(cmds_lst):
+            print("{} - {}".format(c_lst, cmd))
+            split_lst[c_lst]["cmds"].append(cmd)
+            c_lst = c_lst + 1
+            if c_lst >= n_out_files:
+                c_lst = 0
+
+        for out_cmds_file in split_lst:
+            pbpt_txt_utils.writeList2File(out_cmds_file["cmds"], out_cmds_file["file"])
+    else:
+        outfile_id = 1
+        for i in range(n_out_files):
+            l_bound = i * n_out_cmds
+            u_bound = (i + 1) * n_out_cmds
+            outfile_name = '{0}_{1}{2}'.format(outfile_base, outfile_id, outfile_ext)
+            logger.info('Creating file: {}.'.format(outfile_name))
+            pbpt_txt_utils.writeList2File(cmds_lst[l_bound:u_bound], outfile_name)
+            out_file_lst.append(outfile_name)
+            outfile_id = outfile_id + 1
+
+        if n_remain > 0:
+            # Output remaining cmds file
+            l_bound = n_out_files * n_out_cmds
+            u_bound = n_cmds
+            outfile_name = '{0}_{1}{2}'.format(outfile_base, outfile_id, outfile_ext)
+            logger.info('Creating file: {}.'.format(outfile_name))
+            pbpt_txt_utils.writeList2File(cmds_lst[l_bound:u_bound], outfile_name)
+            out_file_lst.append(outfile_name)
 
     out_filelist_name = '{0}_filelst{2}'.format(outfile_base, outfile_id, outfile_ext)
     pbpt_txt_utils.writeList2File(out_file_lst, out_filelist_name)
 
-def splitIntoNFiles(cmds_lst, n_out_files, outputfile):
+def splitIntoNFiles(cmds_lst, n_out_files, outputfile, deal_split=False):
     n_cmds = len(cmds_lst)
     n_cmds_perfile = math.ceil(n_cmds/n_out_files)
     logger.info("There will be {} commands per output file.".format(n_cmds_perfile))
-    splitByNumCommands(cmds_lst, n_cmds_perfile, outputfile)
+    splitByNumCommands(cmds_lst, n_cmds_perfile, outputfile, deal_split)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -92,6 +120,8 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--nfiles", type=int, default=0, help="The number of output files to generate.")
     parser.add_argument("-p", "--precmd", type=str, help="Optionally provide custom command (e.g., singularity) to be "
                                                          "prepended to all the commands.")
+    parser.add_argument("--dealsplit", action='store_true', default=False,
+                        help="Splits the commands as cards would be dealt rather than linear sequential split.")
 
     args = parser.parse_args()
 
@@ -112,6 +142,6 @@ if __name__ == "__main__":
         cmds_lst = tmp_cmds_lst
 
     if args.split > 0:
-        splitByNumCommands(cmds_lst, args.split, args.output)
+        splitByNumCommands(cmds_lst, args.split, args.output, args.dealsplit)
     elif args.nfiles > 0:
-        splitIntoNFiles(cmds_lst, args.nfiles, args.output)
+        splitIntoNFiles(cmds_lst, args.nfiles, args.output, args.dealsplit)
