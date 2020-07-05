99be28b9669f3c92f4890a3a08ccb8d75f26e5f3
Pete Bunting <petebunting@mac.com>
2020-07-05 20:04:11 +0100

    Update to find_first_file() function so user can select whether exception is throw or none returned if a file is not found.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ import setuptools
 import os
 
 setuptools.setup(name='pb_process_tools',
-    version='1.2.3',
+    version='1.2.4',
     description='Tools for batch processing data, including on HPC cluster with slurm.',
     author='Pete Bunting',
     author_email='petebunting@mac.com',
